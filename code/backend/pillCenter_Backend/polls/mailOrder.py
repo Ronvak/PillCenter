379fa563181bca830dd71a626466030d0f8e9771
Ronvak1 <ronvak1@gmail.com>
2023-04-27 17:50:40 +0300

    fixes

--- code/backend/pillCenter_Backend/polls/mailOrder.py
+++ code/backend/pillCenter_Backend/polls/mailOrder.py
@@ -3,10 +3,10 @@ from django.template.loader import render_to_string
 
 
 
-def send_mail_order(user , order ,img):
+def send_mail_order(user , order):
     user.save()
-    msg_plain = render_to_string('orderSummary/order_mail.txt', {'user': user , 'order': order  , 'img' : img})
-    msg_html = render_to_string('orderSummary/order_mail.html', {'user': user , 'order': order ,'img': img})
+    msg_plain = render_to_string('orderSummary/order_mail.txt', {'user': user , 'order': order  })
+    msg_html = render_to_string('orderSummary/order_mail.html', {'user': user , 'order': order })
   
     send_mail(
         'אישור הזמנה PillCenter',
