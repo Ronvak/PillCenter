47060ffa464659e14d7d986fe653988daac5c9a2
noam ezra <noamezra97@gmail.com>
2023-05-26 00:23:10 +0300

    delete sessionsummary and fix

--- code/frontend/pillcenter-frontend/src/App.js
+++ code/frontend/pillcenter-frontend/src/App.js
@@ -19,7 +19,6 @@ import Consultant from "./components/pharmacist/Consultant";
 import VideoRoom from "./components/pharmacist/VideoRoom";
 import React from "react";
 import WaitingRoom from "./components/pharamacist/WaitingRoom";
-import SessionSummary from "./components/sessionSummary/SessionSummary";
 import PatientVideoRoom from "./components/pharamacist/PatientVideoRoom";
 
 const cacheRtl = createCache({
--- code/frontend/pillcenter-frontend/src/components/navs/Navbar.js
+++ code/frontend/pillcenter-frontend/src/components/navs/Navbar.js
@@ -3,9 +3,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
 import AuthContext from "../../context/AuthContext";
 import { useContext } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
