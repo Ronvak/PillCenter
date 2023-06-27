74f30e7bed9a6cb76c342586a2ec16fba8669309
Ron Vaknin <102041936+Ronvak@users.noreply.github.com>
2023-06-27 14:20:27 +0200

    agora

--- code/backend/pillCenter_Backend/polls/api/views.py
+++ code/backend/pillCenter_Backend/polls/api/views.py
@@ -11,6 +11,7 @@ from polls.models import Medicine, Inventory, Products, Vending_machines, Orders
 from django.contrib.auth import get_user_model
 from agora_token_builder import RtcTokenBuilder
 from polls import serializers
+from django.conf import settings
 import time
 
 User = get_user_model()
@@ -186,8 +187,8 @@ def getChannel(request):
 @api_view(['POST'])
 def tokenGenerator(request):
     
-    appId = "AGORA_APP_ID"
-    appCertificate = "AGORA_APP_CERTIFICATE" 
+    appId = settings.AGORA_APP_ID
+    appCertificate = settings.AGORA_APP_CERTIFICATE
     channelName = "main"
     uid = 0
     role = 1
